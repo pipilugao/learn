8b1c6847728b2cdb3a7935ae843e63eaa6cffd0b
gaoyuhao <pipilugao@163.com>
2020-07-02 15:06:54 +0800

    添加第3题

---
+++ Python/03.py
@@ -0,0 +1,9 @@
+# 一个整数，它加上100和加上268后都是一个完全平方数，请问该数是多少？
+# 在10000内判断
+import math
+
+for i in range(10000):
+    a = math.sqrt(i + 100)
+    b = math.sqrt(i + 268)
+    if a % 1 == 0 and b % 1 == 0:
+        print(i)
