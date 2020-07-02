c5ee25d7f866624b219c4e3daa307ef0210bad69
gaoyuhao <pipilugao@163.com>
2020-07-02 14:37:51 +0800

    完成2个题

---
+++ Python/01.py
@@ -0,0 +1,11 @@
+#_*_coding:utf-8_*_
+
+count = 0
+for i in range(1, 5):
+    for j in range(1, 5):
+        for k in range(1, 5):
+            if i != j and j != k and i != k:
+                count += 1
+                print(i, j, k, sep='')
+
+print('count:', str(count))
